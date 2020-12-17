549967c1b812a694afa1cc5ff510a0413d4aaa65
abhisheksharma0904 <76174342+abhisheksharma0904@users.noreply.github.com>
2020-12-18 01:07:48 +0530

    Update Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// TC: O(n), n: number of nodes in the linked list.
+// SC: O(1)
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode fast = head, slow = head;
